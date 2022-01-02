2e81d3aa98d214486226ecf0159a23b78a4b743e
parvathy <parvathyk285@gmail.com>
2022-01-02 21:40:29 +0530

    Command

---
+++ src/main/java/Homework/HomeWork4.java
@@ -0,0 +1,60 @@
+package Homework;
+
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.chrome.ChromeDriver;
+        import org.openqa.selenium.edge.EdgeDriver;
+        import org.openqa.selenium.firefox.FirefoxDriver;
+
+        import java.util.List;
+
+public class HomeWork4 {
+    static WebDriver driver;
+
+    public static void testInitials(String browser) {
+        if (browser.equals("Chrome")) {
+            System.setProperty("webdriver.chrome.driver", "C:\\selenium\\selenium driver\\chromedriver.exe");
+            driver = new ChromeDriver();
+        } else if (browser.equals("Edge")) {
+            System.setProperty("webdriver.edge.driver", "C:\\selenium\\selenium driver\\msedgedriver.exe");
+            driver = new EdgeDriver();
+        } else if (browser.equals("Firefox")) {
+            System.setProperty("webdriver.gecko.driver", "C:\\selenium\\selenium driver\\geckodriver.exe");
+            driver = new FirefoxDriver();
+        } else {
+            try {
+                throw new Exception("Invalid browser name");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+    }
+
+    public static void main(String args[]) {
+        testInitials("Chrome");
+        driver.get("http://demowebshop.tricentis.com/shipping-returns");
+        String actualTitle = driver.getTitle();
+        System.out.println(actualTitle);
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println(currentUrl);
+        String pageSource = driver.getPageSource();
+        WebElement name1 = driver.findElement(By.name("NewsletterEmail"));
+        WebElement name2 = driver.findElement(By.id("newsletter-email"));
+        WebElement name3 = driver.findElement(By.cssSelector("#newsletter-email"));
+        WebElement name4 = driver.findElement(By.xpath("//*[@id=\"newsletter-email\"]"));
+        WebElement name5 = driver.findElement(By.linkText("Shopping cart"));
+        WebElement name6 = driver.findElement(By.partialLinkText("Shopping"));
+        List<WebElement> name7 = driver.findElements(By.tagName("div"));
+        System.out.println(name1);
+        System.out.println(name2);
+        System.out.println(name3);
+        System.out.println(name4);
+        System.out.println(name5);
+        System.out.println(name6);
+        System.out.println(name7.size());
+        driver.close();
+    }
+}
---
+++ src/main/java/Homework/HomeWork5.java
@@ -0,0 +1,59 @@
+package Homework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+
+public class HomeWork5 {
+    static WebDriver driver;
+
+    public static void testInitials(String browser) {
+        if (browser.equals("Chrome")) {
+            System.setProperty("webdriver.chrome.driver", "C:\\selenium\\selenium driver\\chromedriver.exe");
+            driver = new ChromeDriver();
+        } else if (browser.equals("Edge")) {
+            System.setProperty("webdriver.edge.driver", "C:\\selenium\\selenium driver\\msedgedriver.exe");
+            driver = new EdgeDriver();
+        } else if(browser.equals("Firefox")) {
+            System.setProperty("webdriver.gecko.driver", "C:\\selenium\\selenium driver\\geckodriver.exe");
+            driver = new FirefoxDriver();
+        } else {
+            try {
+                throw new Exception("Invalid browser name");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+    }
+    public static void main(String args[]) {
+        testInitials("Chrome");
+        driver.get("http://demowebshop.tricentis.com/tricentis");
+        String actualTitle = driver.getTitle();
+        System.out.println(actualTitle);
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println(currentUrl);
+        String pageSource = driver.getPageSource();
+        WebElement name = driver.findElement(By.name("q"));
+        WebElement name2 = driver.findElement(By.id("small-searchterms"));
+        WebElement name3 = driver.findElement(By.cssSelector("#small-searchterms"));
+        WebElement name4 = driver.findElement(By.xpath("//*[@id=\"small-searchterms\"]"));
+        WebElement name5 = driver.findElement(By.linkText("Tricentis"));
+        WebElement name6 = driver.findElement(By.partialLinkText("Shopping"));
+        List<WebElement> name7 = driver.findElements(By.tagName("div"));
+        System.out.println(name);
+        System.out.println(name2);
+        System.out.println(name3);
+        System.out.println(name4);
+        System.out.println(name5);
+        System.out.println(name6);
+        System.out.println(name7.size());
+        driver.close();
+    }
+    }
