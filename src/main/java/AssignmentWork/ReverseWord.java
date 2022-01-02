be07ce53cf1292f0ea238ed86eda7ed79cb08b96
parvathy <parvathyk285@gmail.com>
2022-01-02 22:02:14 +0530

    Command

---
+++ src/main/java/AssignmentWork/ReverseWord.java
@@ -0,0 +1,12 @@
+package AssignmentWork;
+
+public class ReverseWord {
+    public  static void main(String[] args){
+        String str="Mango";
+        StringBuilder rev= new StringBuilder();
+        int len=str.length();
+        for(int i=0;i<=len-1;i++)
+            rev.insert(0, str.charAt(i));
+        System.out.println("Reversed String:"+rev);
+    }
+}
