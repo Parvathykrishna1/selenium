272f051e1399462f3f293f3532fd550177855dfb
parvathy <parvathyk285@gmail.com>
2022-01-03 22:33:21 +0530

    Command

--- src/main/java/Xpath/Program2.java
+++ src/main/java/Xpath/Program2.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Program1 {
+public class Program2 {
     static WebDriver driver;
 
     public static void testInitials(String browser) {
@@ -34,5 +34,12 @@ public class Program1 {
         testInitials("Chrome");
         driver.get("https://login.yahoo.com/account/create");
         driver.findElement(By.xpath("//input[@id='usernamereg-firstName']")).sendKeys("Your-Name"); // Will send values to First Name tab
+        driver.findElement(By.xpath("//input[@id='usernamereg-lastName']")).sendKeys("Your-Last_name"); //xpath for last name box
+        driver.findElement(By.xpath("//input[@id='usernamereg-yid']")).sendKeys("email@yahoo.com"); //xpath for email box
+        driver.findElement(By.xpath("//input[@id='usernamereg-phone']")).sendKeys("123456789"); //xpath for phone number box
+        driver.findElement(By.xpath("//select[@id='usernamereg-month']")).click(); //xpath for usermonth box
+        driver.findElement(By.xpath("//input[@id='usernamereg-day']")).sendKeys("01"); //xpath for userday box
+        driver.findElement(By.xpath("//input[@id='usernamereg-year']")).sendKeys("1999");// xpath for user year
+        driver.findElement(By.xpath("//button[@id='reg-submit-button']")).click();// xpath for submit button
     }
 }
